deda8e7674c022a58a00cd54f589eda5fa4bba0a
Goktugg0 <goktugcakmakli35@gmail.com>
2025-07-30 22:35:49 +0300

    message sending works

--- script.py
+++ script.py
@@ -1,1 +1,17 @@
-BOT_TOKEN = "7826512571:AAGDi2IP_K0ccQLMKLGkxQZzW8iYDIa-GAM"
+import requests
+
+BOT_TOKEN = ""
+CHAT_ID = ""
+URL = "./siteLink.txt"
+CHECK_INTERVAL = 60  # seconds
+LAST_HASH = None
+
+def send_telegram_message():
+    telegram_url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    payload = {
+        'chat_id': CHAT_ID,
+        'text': "HI"
+    }
+    requests.post(telegram_url, data=payload)
+send_telegram_message()
+
