d3c986f05e04a3403c19cdf32f7b859f2ceefcc4
Goktugg0 <goktugcakmakli35@gmail.com>
2025-07-31 00:28:28 +0300

    successful fetching website

--- script.py
+++ script.py
@@ -1,10 +1,20 @@
 import requests
+from bs4 import BeautifulSoup
 
 BOT_TOKEN = ""
 CHAT_ID = ""
-URL = "./siteLink.txt"
+FILTERED_URL = "https://plaza.newnewnew.space/en/availables-places/living-place#?gesorteerd-op=prijs%2B&locatie=Eindhoven-Nederland%2B-%2BNoord-Brabant"
 CHECK_INTERVAL = 60  # seconds
 LAST_HASH = None
+
+
+def fetch_page_content(url):
+    try:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        print(soup.prettify())  # Print the HTML content for debugging
+    except Exception as e:
+        print(f"Error fetching/parsing page: {e}")
 
 def send_telegram_message():
     telegram_url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
@@ -12,6 +22,10 @@ def send_telegram_message():
         'chat_id': CHAT_ID,
         'text': "HI"
     }
-    requests.post(telegram_url, data=payload)
+    try:
+        requests.post(telegram_url, data=payload)
+    except Exception as e:
+        print(f"Error sending message: {e}")
 send_telegram_message()
+fetch_page_content(FILTERED_URL)
 
