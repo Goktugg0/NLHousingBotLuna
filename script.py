0d18c9d88200317ba26a68f8f363af9f8d568aa6
Goktugg0 <goktugcakmakli35@gmail.com>
2025-08-01 13:55:54 +0300

    somewhat workıng fılter

--- script.py
+++ script.py
@@ -1,5 +1,12 @@
+import time
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+import json
+import os
+import time
 
 BOT_TOKEN = ""
 CHAT_ID = ""
@@ -12,16 +19,23 @@ DIV_CLASS = 'ng-scope object-list-items-container'
 
 def fetch_page_content(url):
     try:
-        response = requests.get(url)
-        soup = BeautifulSoup(response.content, 'html.parser')
-        print(soup.prettify())  # Print the HTML content for debugging
+        options = Options()
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+
+        driver = webdriver.Chrome(options=options)
+        driver.get(url)
+        time.sleep(5)  # Wait for JavaScript to render
+        soup = BeautifulSoup(driver.page_source, "html.parser")
+        driver.quit()
     except Exception as e:
         print(f"Error fetching/parsing page: {e}")
         return None
     return soup
 
 def parse_housing_data(soup):
-    houses = soup.select(DIV_CLASS)
+    houses  = soup.select("section.list-item")
     print(houses)  # Print the housing data for debugging
 
 
@@ -38,7 +52,13 @@ def send_telegram_message():
         print(f"Error sending message: {e}")
 
 
-#send_telegram_message()
-soup = fetch_page_content(FILTERED_URL)
-parse_housing_data(soup)
+def main():
+    #send_telegram_message()
+    soup = fetch_page_content(FILTERED_URL)
+    parse_housing_data(soup)
+
+if __name__ == "__main__":
+    main()
+
+
 
